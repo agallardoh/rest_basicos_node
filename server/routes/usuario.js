bd35d366ef9adcf8d8790d05f9659f2b028db160
Agustin <agustingh1583@gmail.com>
2021-01-31 12:11:39 -0500

    Mostrael total de campos del get

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -25,11 +25,16 @@ app.get('/usuario', function(req, res) {
                     err
                 });
             }
-            res.json({
-                ok: true,
-                usuarios
+            Usuario.count({}, (err, conteo) => {
 
+                res.json({
+                    ok: true,
+                    usuarios,
+                    cuantos: conteo
+
+                });
             });
+
         });
 
 });
