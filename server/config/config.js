65b02eaac5bd16740c8fa14bcdb59f9dfbcbaa34
Agustin Gallardo <agustingh1583@gmail.com>
2021-02-06 09:51:47 -0500

    Generacion JWT

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,20 @@
 //========================
 
 process.env.port = process.env.port || 3000;
+
+
+//========================
+// Vencimiento del token
+//========================
+//60 Segundos
+//60 Minutos
+//24 Horas
+//30 Dias
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+//========================
+// SEED de autenciacion
+//========================
+
+process.env.SEED = process.env.SEED || 'secretDesa';
